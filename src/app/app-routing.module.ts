471b362d17768a056c9586a95a114be8e5aeefe1
MauricioRL15 <15rlmauricio@gmail.com>
2023-10-17 18:20:22 -0600

    pipipi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CashBreakComponent } from './components/cash-break/cash-break.component';
+import { DetailsComponent } from './components/details/details.component';
+import { OrderComponent } from './components/order/order.component';
+import { TablesComponent } from './components/tables/tables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'cash-break', component: CashBreakComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'order', component: OrderComponent },
+  { path: 'tables', component: TablesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
